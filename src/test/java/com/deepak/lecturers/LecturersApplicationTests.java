dd3672e3adaafd7987d63fde2000cb80b6323bdf
Deepak Jain <deepak.jain09886522785+git@gmail.com>
2025-03-18 09:54:49 +0530

    initial commit

---
+++ src/test/java/com/deepak/lecturers/LecturersApplicationTests.java
@@ -0,0 +1,13 @@
+package com.deepak.lecturers;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LecturersApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
